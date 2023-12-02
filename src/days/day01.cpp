1705872aa3513417a1deeb766b8854c1d701085a
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2023-12-02 01:00:44 +0100

    solution Day1 part two

--- src/days/day01.cpp
+++ src/days/day01.cpp
@@ -1,4 +1,5 @@
 #include "include/day01.hpp"
+#include "../utilities.hpp"
 #include <algorithm>
 #include <concepts>
 #include <fstream>
@@ -7,11 +8,24 @@
 #include <numeric>
 #include <ranges>
 #include <vector>
-#include "../utilities.hpp"
 
-auto sumOfCalibrationValues(const std::string& input) -> int
-{ // 54951
+namespace
+{
+const std::unordered_map<std::string, int> valuesToNumber = {
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9}
+    };
+} // namespace
 
+auto trebuchetPartOne(const std::string& input) -> int // correct Answer: = 54951
+{
     auto lines = utils::lines(input);
     int sum = 0;
 
@@ -31,20 +45,85 @@ auto sumOfCalibrationValues(const std::string& input) -> int
     return sum;
 }
 
-auto sumOfCalibrationValuesPart2(const std::string& input)->int
+auto trebuchetPartTwo(const std::string& input) -> int // correct Answer = 55218
 {
-    
-}
+    auto lines = utils::lines(input);
 
-auto Day01::part_one(const std::string& input)->std::string
+    auto isLetter = [&valuesToNumber = valuesToNumber](const std::string& line, int index) -> int
+    {
+        for (const auto& [key, value] : valuesToNumber)
+        {
+            if (key == line.substr(index, key.size()) || line[index] - '0' == value)
+            {
+                return value;
+            }
+        }
+        return -1;
+    };
+
+    int sum{0};
+    for (const std::string& line : lines)
+    {
+        int first = -1;
+        int last = -1;
+        int sizeOfLine = static_cast<int>(line.size());
+        for (int i = 0; i < sizeOfLine; i++)
+        {
+            int letterValue = isLetter(line, i);
+            if (letterValue != -1)
+            {
+                if (first == -1) first = letterValue;
+                last = letterValue;
+            }
+        }
+        assert(first != -1);
+        sum += first * 10 + last;
+    }
+
+    return sum;
+}
+/*
+second Solution for part2
+    std::vector<std::string> numbers{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    for (const std::string& line : lines)
+    {
+        std::vector<int> digits{};
+        for (int i = 0; i < static_cast<int>(line.size()); ++i)
+        {
+            if (std::isdigit(line[i]))
+            {
+                digits.push_back(line[i] - '0');
+            }
+            else if (std::isalpha(line[i]))
+            {
+                for (size_t j = 0; j < numbers.size(); ++j)
+                {
+                    const auto& word = numbers[j];
+                    if (line.size() >= i + word.size() && line.substr(i, word.size()) == word)
+                    {
+                        digits.push_back(j + 1);
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (not digits.empty())
+        {
+            sum += digits.front() * 10 + digits.back();
+        }
+    }
+*/
+
+auto Day01::part_one(const std::string& input) -> std::string
 {
-    int result = sumOfCalibrationValues(input);
+    int result = trebuchetPartOne(input);
     return std::to_string(result);
 }
 
-auto Day01::part_two(const std::string& input)->std::string
+auto Day01::part_two(const std::string& input) -> std::string
 {
-    // int result = sumOfCalibrationValuesPart2(input);
-    // return std::to_string(result);
-    return "foo";
+    int result = trebuchetPartTwo(input);
+    return std::to_string(result);
 }
