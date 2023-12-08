57ee9267786b61e34acc8b16620bf3208e59d383
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2023-12-08 18:49:33 +0100

    preapre for day8

--- src/advent.hpp
+++ src/advent.hpp
@@ -27,6 +27,8 @@ auto puzzle(int day) -> std::unique_ptr<Puzzle>
             return std::make_unique<Day06>(Day06());
         case 7:
             return std::make_unique<Day07>(Day07());
+        case 8:
+            return std::make_unique<Day08>(Day08());
         default:
             std::cout << "ops";
             return nullptr;
---
+++ src/days/day08.cpp
@@ -0,0 +1,14 @@
+#include "include/day08.hpp"
+#include "utilities.hpp"
+
+auto Day08::part_one(const std::string& input) -> std::string
+{
+    auto line = utils::lines(input);
+    utils::printVec(line);
+    return "part one";
+}
+
+auto Day08::part_two(const std::string&) -> std::string // 251135960
+{
+    return "part two";
+}
