abefb4f7a054b45b6af2138c0681b9914e371f8c
Jaroslaw <roszyk.jarek@gmail.com>
2023-12-10 17:27:54 +0100

    Preparation for day10

--- src/advent.hpp
+++ src/advent.hpp
@@ -31,6 +31,8 @@ auto puzzle(int day) -> std::unique_ptr<Puzzle>
             return std::make_unique<Day08>(Day08());
         case 9:
             return std::make_unique<Day09>(Day09());
+        case 10:
+            return std::make_unique<Day10>(Day10());
         default:
             std::cout << "ops";
             return nullptr;
---
+++ src/days/day10.cpp
@@ -0,0 +1,15 @@
+#include "include/day10.hpp"
+#include <cassert>
+#include <numeric>
+#include "utilities.hpp"
+
+auto Day10::part_one(const std::string& input) -> std::string
+{
+    utils::printVec(utils::lines(input));
+    return "part one";
+}
+
+auto Day10::part_two(const std::string&) -> std::string
+{
+    return "part two";
+}
