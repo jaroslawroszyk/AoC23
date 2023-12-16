0c91d7558cf6fb61aeda55e17d0e7b3980792cf8
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2023-12-16 01:57:45 +0100

    solve day3 part2

--- src/days/day03.cpp
+++ src/days/day03.cpp
@@ -4,9 +4,29 @@
 #include <numeric>
 #include <string>
 #include "utilities.hpp"
-
-namespace part1
+namespace
 {
+static auto isdigit(char c) -> bool
+{
+    return '0' <= c && c <= '9';
+}
+
+static auto inbounds(const std::vector<std::string>& lines, int x, int y) -> bool
+{
+    return 0 <= y && y < lines.size() && 0 <= x && x < lines[y].size();
+}
+
+static auto expand(const std::string& line, int start) -> std::pair<int, int>
+{
+    int s = start, e = start;
+    for (; s >= 0 && isdigit(line[s]); --s)
+    {
+    }
+    for (; e < line.size() && isdigit(line[e]); ++e)
+    {
+    }
+    return {s + 1, e};
+}
 auto isSpecial(const std::vector<std::string>& lines)
 {
     return [lines](int64_t row, int64_t col)
@@ -17,7 +37,26 @@ auto isSpecial(const std::vector<std::string>& lines)
     };
 }
 
-auto sumSchemantic(const std::string& input) -> int
+} // namespace
+
+namespace std
+{
+template <>
+struct hash<std::array<int, 3>>
+{
+    size_t operator()(const std::array<int, 3>& arr) const
+    {
+        size_t hashValue = 0;
+        for (int value : arr)
+        {
+            hashValue ^= std::hash<int>()(value) + 0x9e3779b9 + (hashValue << 6) + (hashValue >> 2);
+        }
+        return hashValue;
+    }
+};
+} // namespace std
+
+auto Day03::part1(const std::string& input) -> int
 {
     std::vector<std::string> lines = utils::lines(input);
 
@@ -62,11 +101,63 @@ auto sumSchemantic(const std::string& input) -> int
     }
     return sum;
 }
-} // namespace part1
+
+auto Day03::part2(const std::string& input) -> int
+{
+    auto lines = utils::lines(input);
+    std::vector<std::array<int, 2>> gears;
+
+    for (int y = 0; y < lines.size(); ++y)
+    {
+        const std::string& str = lines[y];
+        for (int x = 0; x < str.size(); ++x)
+        {
+            if (str[x] == '*')
+            {
+                gears.push_back({x, y});
+            }
+        }
+    }
+
+    const std::array<std::array<int, 2>, 8> neighbours = {
+        {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}}};
+
+    int sum = 0;
+
+    for (const auto& pt : gears)
+    {
+        std::unordered_set<std::array<int, 3>> numbers;
+        int x = pt[0], y = pt[1];
+
+        for (const auto& neighbour : neighbours)
+        {
+            int dx = neighbour[0], dy = neighbour[1];
+
+            if (inbounds(lines, x + dx, y + dy) && isdigit(lines[y + dy][x + dx]))
+            {
+                auto [s, e] = expand(lines[y + dy], x + dx);
+                numbers.insert({s, e, y + dy});
+            }
+        }
+
+        if (numbers.size() == 2)
+        {
+            int product = 1;
+            for (const auto& n : numbers)
+            {
+                int s = n[0], e = n[1], y = n[2];
+                product *= utils::parse<int>(lines[y].substr(s, e - s));
+            }
+            sum += product;
+        }
+    }
+
+    return sum;
+}
 
 auto Day03::part_one(const std::string& input) -> std::string
 {
-    auto result = part1::sumSchemantic(input);
+    auto result = part1(input);
     auto expectedResult{553079};
     assert(result == expectedResult);
     return std::to_string(result);
@@ -74,5 +165,8 @@ auto Day03::part_one(const std::string& input) -> std::string
 
 auto Day03::part_two(const std::string& input) -> std::string
 {
-    return "part_two";
+    auto result = part2(input);
+    auto expectedResult{84363105};
+    assert(result == expectedResult);
+    return std::to_string(result);
 }
--- src/days/include/day03.hpp
+++ src/days/include/day03.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include <string>
+#include <vector>
 #include "../Puzzle.hpp"
 
 struct Day03 : Puzzle
 {
+public:
     auto part_one(const std::string&) -> std::string override;
     auto part_two(const std::string&) -> std::string override;
+
+private:
+    auto part1(const std::string&) -> int;
+    auto part2(const std::string& input) -> int;
 };
