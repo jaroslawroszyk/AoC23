4732aac07cddbf092cc281c597cd8f8b0b8f53aa
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2023-12-18 23:38:16 +0100

    feat: add operator to point class

--- src/utilities/utilities.hpp
+++ src/utilities/utilities.hpp
@@ -16,6 +16,16 @@ struct Point
 
     Point(int64_t x, int64_t y) : x(x), y(y) {}
     Point() = default;
+
+    Point operator+(const Point& other) const { return {x + other.x, y + other.y}; }
+
+    Point operator-(const Point& other) const { return {x - other.x, y - other.y}; }
+
+    Point operator*(int scalar) const { return {x * scalar, y * scalar}; }
+
+    bool operator==(const Point& other) const { return x == other.x && y == other.y; }
+
+    bool operator!=(const Point& other) const { return !(*this == other); }
 };
 
 namespace helper
